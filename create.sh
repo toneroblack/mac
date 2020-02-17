4254f533563be9d26f2856dcd3177636520b8f50
Anthony Inope <inopeanthony@gmail.com>
2020-02-17 06:54:10 -0800

    Added NAT Gateway to the public Subnet with and Elastic IP

--- create.sh
+++ create.sh
@@ -2,3 +2,4 @@ aws cloudformation create-stack \
 --stack-name $1 \
 --parameters file://$2 \
 --template-body file://$3 \
+--region us-west-2 
--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@ aws cloudformation update-stack \
 --stack-name $1 \
 --parameters file://$2 \
 --template-body file://$3 \
-region=us-west-2
+--region us-west-2
